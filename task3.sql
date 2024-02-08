5ce4e2832c8efbd87b9bc4ea8eafb4c8ceed7af1
ZuzkaHas <hasanova.zuzana@gmail.com>
2024-02-08 22:44:57 +0100

    select distinct added-value in each year is counted only once

--- task3.sql
+++ task3.sql
@@ -1,9 +1,15 @@
 SELECT 
+x.product_name,
+round (sum (x.price_difference_in_percentage), 2) AS total_price_difference_in_percentage
+FROM (
+SELECT DISTINCT 
 a.product_name, 
-round (sum (((a.avg_product_value-b.avg_product_value)/b.avg_product_value)*100), 0) AS total_price_difference_in_percentage
+a.calendar_year,
+((a.avg_product_value-b.avg_product_value)/b.avg_product_value)*100 AS price_difference_in_percentage
 FROM t_zuzana_hasanova_project_SQL_primary_final AS a
 LEFT JOIN t_zuzana_hasanova_project_SQL_primary_final AS b
 ON a.calendar_year = b.calendar_year + 1
 AND a.product_name = b.product_name
-GROUP BY a.product_name
+) AS x
+GROUP BY x.product_name
 ORDER BY total_price_difference_in_percentage;
